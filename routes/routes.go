e386e448eef0002c19ac2dda9c53a60506e8062a
PogunGun <pavel.pogulailo@gmail.com>
2023-07-06 23:05:52 +0300

    update userInfo, userPassword

--- routes/routes.go
+++ routes/routes.go
@@ -13,18 +13,24 @@ func Setup(app *fiber.App) {
 	app.Use(middleware.IsAuthenticated)
 	app.Get("/api/user", controllers.User)
 	app.Post("/api/logout", controllers.LogOut)
+
 	// CRUD USER
+	app.Put("/api/users/info", controllers.UpdateInfo)
+	app.Put("/api/users/password", controllers.UpdatePassword)
+
 	app.Get("/api/users", controllers.AllUser)
 	app.Post("/api/users", controllers.CreateUser)
 	app.Get("/api/users/:id", controllers.GetUser)
 	app.Put("/api/users/:id", controllers.UpdateUser)
 	app.Delete("/api/users/:id", controllers.DeleteUser)
+
 	// CRUD Role
 	app.Get("/api/roles", controllers.AllRoles)
 	app.Post("/api/roles", controllers.CreateRole)
 	app.Get("/api/roles/:id", controllers.GetRole)
 	app.Put("/api/roles/:id", controllers.UpdateRole)
 	app.Delete("/api/roles/:id", controllers.DeleteRole)
+
 	//Permission
 	app.Get("/api/permissions", controllers.AllPermissions)
 }
